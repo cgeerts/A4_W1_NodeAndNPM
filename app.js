a571502164715a8b72cd39b1797c9e2989ee8a9b
cgeerts <c_geerts102015@fanshaweonline.ca>
2021-01-19 12:20:33 -0500

    fixes

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http'); // like a PHP require
+
+// require is more or less the same as a JS import
+
+const hostname = '127.0.0.1'; // this is localhost
+const port = 3000; // localhost:3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, () => {
+  console.log(`Server is now running at http://${hostname}:${port}/`);
+});
